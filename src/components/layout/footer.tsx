d55bd20b1e6dd877b92e172a685f2a4f9d967dfe
Ndombi-Gnele Franck <allanfranckgnele@gmail.com>
2026-04-04 22:41:55 +0200

    feat: ajout de la page d'accueil (#12)

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Footer(){
+    return (
+        <footer className="bg-[#2D3436] text-white py-8 px-6">
+            <div className="max-w-7xl mx-auto text-center">
+            <p className="font-light opacity-70">© 2026 Cabinet de Kinésithérapie - Tous droits réservés</p>
+            </div>
+        </footer>
+    );
+}
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header >
+      <div >
+
+          <nav className="fixed top-0 w-full h-25 bg-white/80 backdrop-blur-md z-50 border-b border-gray-100">
+          <div  className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+            {/* <Link href="/">
+              <Image
+                src="/"
+                alt=""
+                width={120}
+                height={50}
+
+              />
+            </Link> */}
+          <div className="hidden md:block text-xxl">
+              <ul className="flex gap-8">
+                <li className="text-[#2D3436] hover:text-[#7C9885] transition-colors">
+                  <Link href="/prestations">Services</Link>
+                </li>
+                <li className="text-[#2D3436] hover:text-[#7C9885] transition-colors">
+                  <Link href="/cabinet">Le cabinet</Link>
+                </li>
+                <li className="text-[#2D3436] hover:text-[#7C9885] transition-colors">
+                  <Link href="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+              <button className="bg-[#7C9885] hover:bg-[#6B8574] text-white rounded-full px-6 py-4"><Link href="/reservation" >
+              Prendre RDV
+              </Link></button>
+            
+
+          </div>
+          
+            
+          
+        </nav >
+      </div>
+    </header>
+  );
+}
