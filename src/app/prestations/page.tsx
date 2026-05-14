a8347176057af081ff30cd6ed4097f962ca7e6f5
Franck Allan NG <kaizerx438@gmail.com>
2026-05-14 15:11:15 +0200

    feat: page prestations - en attente infos client

--- src/app/prestations/page.tsx
+++ src/app/prestations/page.tsx
@@ -1,3 +1,18 @@
+import { section } from "framer-motion/client";
+
 export default function PrestationsPage() {
-  return <div>Prestations Page</div>;
+  return (
+
+  <>
+  <section>
+      <div>Prestations Page</div>;
+
+  </section>
+  
+  </>
+
+
+  )
+  
+
 }
